8f22fc6b6f793ac0f22db277f7693f254de8482c
Dennis Adam <dennis.adam95@gmail.com>
2024-01-31 02:31:37 +0100

    read ical file

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+import icalendar
+from icalendar import Calendar
+from datetime import datetime
+import os
+
+# def print_hi(name):
+#     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+
+    # init the calendar
+    cal = Calendar()
+
+    # configuration
+    cal.add('version', '2.0')
+
+    # open file
+    file = open('./data/Abfuhrtermine.ics', 'rb')
+    filecal = icalendar.Calendar.from_ical(file.read())
+
+    for event in filecal.walk('vevent'):
+        print('Desc: ', event.get('description'))
+        print('Start: ', event.decoded('dtstart'))
+        print('End: ', event.decoded('dtend'))
+
+    file.close()
