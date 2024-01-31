b8381dcce7db08c5b6374f2a43a8de7804fc7623
Dennis Adam <dennis.adam95@gmail.com>
2024-01-31 02:48:53 +0100

    added further todos - raw in the file for now

--- main.py
+++ main.py
@@ -26,3 +26,9 @@ if __name__ == '__main__':
         print('End: ', event.decoded('dtend'))
 
     file.close()
+
+# TODO:
+#  - process only events in the future
+#  - show windows notification one day before the event
+#       - show description, which trash and custom text like tomorrow or in two days for example
+#  - make a test ical for testing (generated every time i run this script with dates for tomorrow, in two days and 2 or 3 further in the future?!)
